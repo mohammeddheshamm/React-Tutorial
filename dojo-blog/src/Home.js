ea7e48c9f6151607ba9fa5965caa4f163913fe22
mohammeddhishamm <m.hesham@Rahminco.com>
2025-03-01 13:50:16 +0200

    The tutorial till the use hook

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+    
+    const [blogs , setBlogs] = useState([
+        {title:"New wesite", body:"lorem ipsum...", author:"Mohamed", id: 1 },
+        {title:"Welcome party!", body:"lorem ipsum...", author:"Hesham", id: 2},
+        {title:"Web dev top tips", body:"lorem ipsum...", author:"Mohamed", id: 3}
+    ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return ( 
         <div className="Home">
-            <h2>Home Page</h2>
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            {/* <BlogList blogs={blogs.filter((blog) => blog.author === "Mohamed")} title="Mohamed's Blogs!"/> */}
         </div>
     );
 }
